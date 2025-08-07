134d6869ddb72d8b79b04892a862809782920fb8
Sylva_q <yiting.sven@gmail.com>
2025-08-07 11:28:23 +0200

    Initial commit

---
+++ implementing_luhn_algorithm.py
@@ -0,0 +1,4 @@
+#The Luhn Algorithm algorithm is a formula to validate a variety of identification numbers.#
+#Start by declaring a function called main, this will serve as the entry point of the program. Use the pass keyword to avoid an error.#
+def main():
+    pass
